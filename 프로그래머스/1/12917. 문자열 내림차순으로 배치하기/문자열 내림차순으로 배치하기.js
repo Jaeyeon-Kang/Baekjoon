5bbf6348f9810bf6f74738924ebbe5469eb3d186
Kang Jaeyeon <93374962+Jaeyeon-Kang@users.noreply.github.com>
2025-02-27 21:14:26 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,10 +1,4 @@
 function solution(s) {    
-   let arr = []
-   for(x of s){
-       let [a,b] = [x, x.charCodeAt()]
-       arr.push([a,b])
-   }
     
-    
-    return arr.sort((a,b)=>b[1]-a[1]).map(x => x[0]).join('')
+    return s.split('').sort().reverse().join('')
 }
